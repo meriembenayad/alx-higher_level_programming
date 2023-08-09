92838f221d4729be0c5259c548b09f2e46d8502e
meriembenayad <meriem.benayad@gmail.com>
2023-08-09 02:59:23 +0100

    print_tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for letter in range(ord('z'), ord('a') -1, -1):
+for letter in range(ord('z'), ord('a') - 1, -1):
     if letter % 2 != 0:
         letter -= 32
     print("{}".format(chr(letter)), end="")
