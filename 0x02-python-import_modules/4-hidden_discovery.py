c37f20d6187b500b84c1cbd87dd285c01bbe54ea
meriembenayad <meriem.benayad@gmail.com>
2023-08-12 01:19:14 +0100

    Tasks: 4 - 103

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    listname = dir(hidden_4)
+    for name in listname:
+        if name[:2] == "__":
+            print(name)
---
+++ 0x02-python-import_modules/easy_print.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def easyprint():
+    print("#pythoniscool")
+easyprint()
