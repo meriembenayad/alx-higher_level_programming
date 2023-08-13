a67d59ef6beaa5f999ec6324bade84c6c151d947
meriembenayad <meriem.benayad@gmail.com>
2023-08-13 23:39:59 +0100

    Fix Task 6 - 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first_char = sentence[0]
     if length == 0:
         return 0, None
     else:
+        first_char = sentence[0]
         return length, first_char
