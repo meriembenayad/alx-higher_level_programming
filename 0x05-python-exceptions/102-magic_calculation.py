e19bd0e8c8d9382bdc1debc9765451193c1bdf84
meriembenayad <meriem.benayad@gmail.com>
2023-08-29 00:13:03 +0100

    Advanced Task: 9

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += a ** b / ele
-        except Exception:
-            result += b + a
+        except:
+            result = b + a
             break
     return result
