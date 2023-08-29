8864e704071fa19f461cf7a8790be3a1dcd4e0b9
meriembenayad <meriem.benayad@gmail.com>
2023-08-30 00:30:51 +0100

    Fix Task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -57,9 +57,9 @@ class Square:
             print in stdout the square with the character #
         """
         if self.__size == 0:
-            print("")
+            print()
         else:
             for i in range(self.__size):
                 for j in range(self.__size):
-                    print("#", end="")
+                    print("#" * self.__size, end="")
                 print("")
