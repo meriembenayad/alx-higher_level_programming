0cc5b336f6d6ff682ed2ae1b86080da1c4f2d60a
meriembenayad <meriem.benayad@gmail.com>
2023-09-12 02:39:28 +0100

    Fix Tasks 0 - 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -19,6 +19,4 @@ def inherits_from(obj, a_class):
     """
     if type(obj) is a_class:
         return False
-
-    if issubclass(type(obj), a_class):
-        return True
+    return issubclass(type(obj), a_class)
