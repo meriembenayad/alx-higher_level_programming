8e1eebe0af255f4eb1e785d60edd54ae3ffd6725
meriembenayad <meriem.benayad@gmail.com>
2023-09-12 02:36:35 +0100

    Fix Tasks 0 - 4

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-   function that returns the list of available 
-   attributes and methods of an object
-"""
+""" Returns the list of available attributes and methods of an object """
 
 
 def lookup(obj):
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -17,7 +17,7 @@ def inherits_from(obj, a_class):
         Return:
             True || False
     """
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return False
 
     if issubclass(type(obj), a_class):
