2c4b72ea989792fbf1299f4438098eb1ff1fcef7
meriembenayad <meriem.benayad@gmail.com>
2023-10-18 20:16:37 +0100

    Task 14, 15, 16 (FIX)

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,7 +4,7 @@
 -- Sorted in ascending order by the genre name
 -- Use only one SELECT statement
 SELECT name FROM tv_genres
-JOIN tv_shows_genres ON id = tv_shows_genres.genre_id
-JOIN tv_shows ON tv_shows.id = tv_shows_genres.show_id
+JOIN tv_shows_genres ON id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY name ASC;
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,6 +4,6 @@
 -- Sorted in ascending order by the show title and genre name
 -- Use only one SELECT statement
 SELECT title, tv_genres.name FROM tv_shows
-LEFT JOIN tv_shows_genres ON id = tv_shows_genres.show_id
-LEFT JOIN tv_genres ON tv_shows_genres.genre_id = tv_genres.id
+LEFT JOIN tv_show_genres ON id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY title, tv_genres.name ASC;
