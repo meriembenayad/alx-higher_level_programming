9ab331f5ed40f95989e9808aa15792cf4326ffb2
meriembenayad <meriem.benayad@gmail.com>
2023-10-18 20:17:49 +0100

    Task 14, 15 (FIX)

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,7 +4,7 @@
 -- Sorted in ascending order by the show title
 -- Use only one SELECT statement
 SELECT title FROM tv_shows
-JOIN tv_shows_genres ON id = tv_show_genres.show_id
+JOIN tv_show_genres ON id = tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY title ASC;
