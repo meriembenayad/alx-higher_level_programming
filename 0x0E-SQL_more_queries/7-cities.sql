04035ab7545c737d671e8239a7f10a2bb1e8406d
meriembenayad <meriem.benayad@gmail.com>
2023-10-18 19:30:47 +0100

    Task 7 - ADD FOREIGN KEY

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,13 @@
 -- Creates the database hbtn_0d_usa and the table cities.
 -- id INT auto generated, not null, primary key.
+-- state_id INT, not null, foreign key to id of states table.
 -- name VARCHAR(256) not null.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    state_id INT not NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
