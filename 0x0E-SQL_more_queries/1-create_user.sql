b853baaca57f0f7a6135abe7a2082d6566ed547d
meriembenayad <meriem.benayad@gmail.com>
2023-10-18 18:59:44 +0100

    Task 1, 2 - Root user & read user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- Creates the MySQL server user user_0d_1.
+-- user_0d_1 should have all privileges.
+-- user_0d_1 password should be set to user_0d_1_pwd.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
