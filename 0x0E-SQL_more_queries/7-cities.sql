343baef1bda9fafc925fc873d944f36b37277dbe
meriembenayad <meriem.benayad@gmail.com>
2023-10-18 19:35:17 +0100

    Task 7 - ADD FOREIGN KEY

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,9 +5,10 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT UNIQUE NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    state_id INT not NULL,
+    id INT AUTO_INCREMENT NOT NULL,
+    UNIQUE (id),
     PRIMARY KEY (id),
+    name VARCHAR(256) NOT NULL,
+    state_id INT NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
