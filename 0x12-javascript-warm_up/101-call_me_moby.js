15de9bdaa5dfa286fa2232ef0422fe933a3ef37c
meriembenayad <meriem.benayad@gmail.com>
2023-11-14 02:19:47 +0100

    Task 15: Add me maybe

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-function callMeMoby (x, theFunction) {
+exports.callMeMoby = (x, theFunction) => {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
 }
-module.exports.callMeMoby = callMeMoby;
