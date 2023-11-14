bcfc668a1bdb5506e0d10774a6373c75d7f2c8ab
meriembenayad <meriem.benayad@gmail.com>
2023-11-14 20:16:34 +0100

    Task 13: Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-let fileA = fs.readFileSync(process.argv[2], 'utf8');
-let fileB = fs.readFileSync(process.argv[3], 'utf8');
+const fileA = fs.readFileSync(process.argv[2], 'utf8');
+const fileB = fs.readFileSync(process.argv[3], 'utf8');
 
 fs.writeFileSync(process.argv[4], fileA + fileB);
