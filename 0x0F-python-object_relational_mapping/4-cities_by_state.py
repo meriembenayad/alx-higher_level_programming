1b7856b9340d2c8616ba37d015deba8e4c791539
meriembenayad <meriem.benayad@gmail.com>
2023-11-18 18:18:12 +0100

    Task 4: Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+   script that lists all cities from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT cities.id, cities.name, states.name\
+        FROM cities\
+        JOIN states ON cities.state_id = states.id\
+        ORDER BY cities.state_id ASC"
+    )
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
