d83f9877b89cc17a547d3936d2d69b2e5189bae8
meriembenayad <meriem.benayad@gmail.com>
2023-11-19 01:54:03 +0100

    Task 12: Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+    script that changes the name of a State object
+    from the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    id_update = 2
+    new_state_name = "New Mexico"
+
+    update_state = session.query(State).filter(State.id == id_update).first()
+    update_state.name = new_state_name
+
+    session.commit()
+    session.close()
