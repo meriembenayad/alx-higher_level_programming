3b111f7b1dd76806788f3040f14034043f485850
meriembenayad <meriem.benayad@gmail.com>
2023-11-30 18:01:41 +0100

    Tasks 3, 4 & 5

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sH "X-School-User-ID: 98" "@1" 
+curl -sH "X-School-User-ID: 98" "$@" 
