fb94bad1b0b399b97e27f9332c70011d9d24f46c
meriembenayad <meriem.benayad@gmail.com>
2023-11-30 19:25:21 +0100

    Task 6: Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" Find a peak module """
+
+
+def find_peak(list_of_integers):
+    """ Finds a peak in a list of unsorted integers """
+    if not list_of_integers:
+        return None
+
+    length = len(list_of_integers)
+    if length == 1:
+        return list_of_integers[0]
+    elif length == 2:
+        return max(list_of_integers)
+
+    for i in range(0, length - 1):
+        if list_of_integers[i] >= list_of_integers[i - 1]\
+                and list_of_integers[i] >= list_of_integers[i + 1]:
+            return list_of_integers[i]
+
+    return max(list_of_integers[0], list_of_integers[i - 1])
