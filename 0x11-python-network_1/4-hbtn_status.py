854abfc8887fb7f62c38267908064073dee3cd81
meriembenayad <meriem.benayad@gmail.com>
2023-12-01 22:56:44 +0100

    Task 4: What's my status #1?

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+    Script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+if __name__ == "__main__":
+    url = requests.get('https://alx-intranet.hbtn.io/status')
+    body = url.text
+
+    print('Body response:')
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body))
