8ecb9c527a32f5a1f1b07e08cef54e88ae3f11b6
meriembenayad <meriem.benayad@gmail.com>
2023-12-01 20:18:13 +0100

    Task 0: What's my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
         body = response.read()
 
         print('Body response:')
-        print('\t - type: {}'.format(type(body)))
-        print('\t - content: {}'.format(body))
-        print('\t - utf8 content: {}'.format(body.decode('utf-8')))
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
+        print('\t- utf8 content: {}'.format(body.decode('utf-8')))
