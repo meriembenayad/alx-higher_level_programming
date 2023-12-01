2e8bf609301a9c268898502a65c3eb4fc3907002
meriembenayad <meriem.benayad@gmail.com>
2023-12-01 14:51:46 +0100

    Task 6: Find peak (FIX)

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,15 +7,5 @@ def find_peak(list_of_integers):
     if not list_of_integers:
         return None
 
-    length = len(list_of_integers)
-    if length == 1:
-        return list_of_integers[0]
-    elif length == 2:
-        return max(list_of_integers)
-
-    for i in range(0, length - 1):
-        if list_of_integers[i] >= list_of_integers[i - 1]\
-                and list_of_integers[i] >= list_of_integers[i + 1]:
-            return list_of_integers[i]
-
-    return max(list_of_integers[0], list_of_integers[i - 1])
+    list_of_integers.sort()
+    return list_of_integers[-1]
