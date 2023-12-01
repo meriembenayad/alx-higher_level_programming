9b2d5ff28e68f8c6da78694e9dac8ab1708185a4
meriembenayad <meriem.benayad@gmail.com>
2023-12-01 18:48:49 +0100

    Task 0: What's my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" Module to fetch status from URL """
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+    print('Body response:')
+    print('\t - type: {}'.format(type(body)))
+    print('\t - content: {}'.format(body))
+    print('\t - utf-8 content: {}'.format(body.decode('utf-8')))
