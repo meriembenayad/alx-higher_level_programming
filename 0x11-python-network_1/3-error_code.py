fb6790e457e55c86fe63f29d195666d3f0c41d22
meriembenayad <meriem.benayad@gmail.com>
2023-12-01 22:28:12 +0100

    Task 3: Error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+    Script that takes in a URL
+    Sends a request to the URL
+    Displays the body of the response (decoded in utf-8)
+"""
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(body)
+    except urllib.error.URLError as e:
+        print("Error code: {}".format(e.code))
