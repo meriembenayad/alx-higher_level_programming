48d31a7cf749d280f261d5323451cc852b6c58e2
meriembenayad <meriem.benayad@gmail.com>
2023-12-02 00:18:52 +0100

    Task 7: Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+    Script that takes in a URL
+    Sends a request to the URL
+    Displays the body of the response.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    url_req = requests.get(url)
+
+    url_status = url_req.status_code
+
+    if url_status == requests.codes.ok:
+        body = url_req.text
+        print(body)
+    else:
+        print('Error code: {}'.format(url_status))
