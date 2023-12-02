3c99bad0860601bcae8a15495776af4b4f58246b
meriembenayad <meriem.benayad@gmail.com>
2023-12-02 01:05:04 +0100

    Task 8: Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+    Script that takes in a letter
+    Sends a POST request to http://0.0.0.0:5000/search_user
+    with the letter as a parameter.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    post_req = requests.post(
+        'http://0.0.0.0:5000/search_user', data={"q": letter})
+
+    try:
+        data = post_req.json()
+        if data:
+            print("[{}] {}".format(data.get("id"), data.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
