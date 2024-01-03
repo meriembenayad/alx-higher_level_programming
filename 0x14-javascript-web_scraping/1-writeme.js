5e456c00b22b53b23ef15b4fb7713543ae72cc47
meriembenayad <meriem.benayad@gmail.com>
2024-01-04 00:47:45 +0100

    Task 1: Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+/* 1. Write a string to a file */
+const fs = require('fs');
+const filePath = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(filePath, string, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  return;
+});
