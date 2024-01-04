07f4b90f6c674c67a6a25acd0eaae880bc79edc5
meriembenayad <meriem.benayad@gmail.com>
2024-01-04 02:38:57 +0100

    Task 5: Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+/* 5. Gets content of webpage
+and stores it in a file */
+const request = require('request');
+const fs = require('fs');
+const urlPage = process.argv[2];
+const filePath = process.argv[3];
+
+request(urlPage, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  } else {
+    console.log(error);
+  }
+});
