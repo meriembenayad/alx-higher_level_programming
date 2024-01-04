2be851ec3c51a60eb6156945861bd3e46bb01a17
meriembenayad <meriem.benayad@gmail.com>
2024-01-04 15:19:41 +0100

    Task 7: Who was playing in this movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+/* 7. Prints all characters of Star Wars movie */
+const request = require('request');
+const movieId = process.argv[2];
+const urlApi = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(urlApi, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body).characters;
+    
+    data.forEach(character => {
+      request(character, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
+        else {
+          console.log(error);
+        }
+      });
+    });
+  } else {
+    console.log(error);
+  }
+});
