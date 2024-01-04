eec25270808bb9e25641ee75ed1cab04f51f3701
meriembenayad <meriem.benayad@gmail.com>
2024-01-04 01:09:45 +0100

    Task 2: Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+/* 2. Return Status Code of a GET Request */
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
