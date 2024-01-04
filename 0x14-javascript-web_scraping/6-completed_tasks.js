1b786432ca7ef160f9630ffad09dc3317d3b0dad
meriembenayad <meriem.benayad@gmail.com>
2024-01-04 03:08:28 +0100

    Task 6: How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+/* 6. Computes the number of tasks completed by user id. */
+const request = require('request');
+const urlApi = process.argv[2];
+
+request(urlApi, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const completeTasks = {};
+
+    for (const task of data) {
+      if (task.completed) {
+        if (completeTasks[task.userId]) {
+          completeTasks[task.userId]++;
+        } else {
+          completeTasks[task.userId] = 1;
+        }
+      }
+    }
+    console.log(completeTasks);
+  } else {
+    console.log(error);
+  }
+});
