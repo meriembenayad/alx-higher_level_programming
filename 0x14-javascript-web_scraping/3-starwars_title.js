9020e89cfd6cb7e0dd23ec4d478762d35ccdf983
meriembenayad <meriem.benayad@gmail.com>
2024-01-04 01:29:47 +0100

    Task 3: Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+/* 3. Print the title of Star Wars movie
+where the episode number matches a given integer
+*/
+const request = require('request');
+const episode = process.argv[2];
+
+const endPoint = `https://swapi-api.alx-tools.com/api/films/${episode}`;
+
+request(endPoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
+});
