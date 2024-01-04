cc35970ac3386e6a2f8063c71183258d05a795c0
meriembenayad <meriem.benayad@gmail.com>
2024-01-04 02:04:36 +0100

    Task 4: Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,6 +7,8 @@ const apiUrl = process.argv[2];
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.log(error);
+  } else if (response.statusCode !== 200) {
+    console.log(error);
   } else {
     const data = JSON.parse(body);
     let count = 0;
