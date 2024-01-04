1b5822112b39934bfaca0d8bf804d3efdaaf9dc3
meriembenayad <meriem.benayad@gmail.com>
2024-01-04 15:38:45 +0100

    Task 8: Righ Order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,27 +1,22 @@
 #!/usr/bin/node
-/* 8. Prints all characters of Star Wars movie
-in the same order of the list “characters” in the /films/ */
+/* 7. Prints all characters of Star Wars movie */
 const request = require('request');
 const movieId = process.argv[2];
 const urlApi = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-function printChar (characters, idx) {
-  request(characters[idx], (err, res, body) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(JSON.parse(body).name);
-      if (idx + 1 < characters.length) {
-        printChar(characters, idx + 1);
-      }
-    }
-  });
-}
-
 request(urlApi, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const data = JSON.parse(body).characters;
-    printChar(data, 0);
+
+    data.forEach(character => {
+      request(character, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        } else {
+          console.log(error);
+        }
+      });
+    });
   } else {
     console.log(error);
   }
