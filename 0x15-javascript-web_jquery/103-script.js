c15a484c55d62c46c8ae473582b71a2476a36ca9
meriembenayad <meriem.benayad@gmail.com>
2024-01-10 21:11:54 +0100

    Task 13: And Press Enter

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+/* global $ */
+/* JavaScript script that fetches and prints how to say “Hello”
+  depending on the language
+ */
+$(document).ready(function () {
+  $('INPUT#btn_translate').click(translate);
+
+  function translate () {
+    const lang = $('INPUT#language_code').val();
+    const url = `https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
+
+    $.getJSON(url, function (data) {
+      $('DIV#hello').text(`${data.hello}`);
+    });
+  }
+
+  $('INPUT#language_code').keypress(function (e) {
+    if (e.which === 13) {
+      translate();
+    }
+  });
+});
