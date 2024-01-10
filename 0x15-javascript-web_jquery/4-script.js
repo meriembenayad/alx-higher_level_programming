241b7175cec9e16b7fd1e76a459a0765f0954d7c
meriembenayad <meriem.benayad@gmail.com>
2024-01-10 19:07:47 +0100

    Task 4 & 5: Change class & Add element

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+/* global $ */
+/* JavaScript script that toggles the class of the <header>
+  when the user clicks on the tag DIV#toggle_header
+ */
+$(document).ready(function () {
+  $('DIV#toggle_header').click(function () {
+    if ($('header').hasClass('red')) {
+      $('header').removeClass('red').addClass('green');
+    } else {
+      $('header').removeClass('green').addClass('red');
+    }
+  });
+});
--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+/* global $ */
+/* JavaScript script that adds a <li> element to a list
+  when the user clicks on the tag DIV#add_item
+ */
+$(document).ready(function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+});
