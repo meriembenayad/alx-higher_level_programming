7c26dea76155b529c2e69af9558cfd76f37fab10
meriembenayad <meriem.benayad@gmail.com>
2024-01-10 19:52:19 +0100

    Task 9: fetch value of hello from url

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+/* global $ */
+/* JavaScript script that fetches from https://hellosalut.stefanbohacek.dev/?lang=fr
+  and displays the value of hello from that fetch in the HTML tag DIV#hello
+  */
+$(document).ready(function () {
+  const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+  $.getJSON(url, function (data) {
+    $('DIV#hello').text(`${data.hello}`);
+  });
+});
