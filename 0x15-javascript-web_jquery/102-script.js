c5533ba46e495661fddea6175908939db2e7988c
meriembenayad <meriem.benayad@gmail.com>
2024-01-10 20:44:50 +0100

    Task 12: Say hello to everybody && git push -e ;

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+/* global $ */
+/* JavaScript script that fetches and prints how to say “Hello”
+  depending on the language
+ */
+$(document).ready(function () {
+  $('INPUT#btn_translate').click(function () {
+    const lang = $('INPUT#language_code').val();
+    const url = `https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
+
+    $.getJSON(url, function (data) {
+        $('DIV#hello').text(`${data.hello}`); 
+    });
+  });
+});
