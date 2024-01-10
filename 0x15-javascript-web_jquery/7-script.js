2427319eb6f7788ffa38a5f3d7433435edec3343
meriembenayad <meriem.benayad@gmail.com>
2024-01-10 19:43:23 +0100

    Task 8: fetches list of movies with adding <li> + Fix style Task 7

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -4,7 +4,7 @@
   from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json
  */
 $(document).ready(function () {
-  const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json'
+  const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
   $.getJSON(url, function (data) {
     $('#character').text(data.name);
   });
--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+/* global $ */
+/* JavaScript script that fetches and lists the title for all movies
+  by using this URL: https://swapi-api.alx-tools.com/api/films/?format=json
+  */
+$(document).ready(function () {
+  const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
+  $.getJSON(url, function (data) {
+    const movies = data.results;
+    $.each(movies, function (idx, movie) {
+      $('UL#list_movies').append(`<li>${movie.title}</li>`);
+    });
+  });
+});
