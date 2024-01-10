0d84a08320e1b496db29f6efe1b5e5aa95306572
meriembenayad <meriem.benayad@gmail.com>
2024-01-10 19:20:57 +0100

    Task 7: fetch character name from url api

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+/* global $ */
+/* JavaScript script that fetches the character name
+  from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json
+ */
+$(document).ready(function () {
+  const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json'
+  $.getJSON(url, function (data) {
+    $('#character').text(data.name);
+  });
+});
