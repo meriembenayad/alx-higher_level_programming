7c1d63ed6ccd1268b843ee142a56071eb5d862d6
meriembenayad <meriem.benayad@gmail.com>
2024-01-10 18:53:41 +0100

    Task 2 & 3: click function and addClass

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+/* global $ */
+/* JavaScript script that updates the text color of the <header>
+  when the user clicks on the tag DIV#red_header
+ */
+$(document).ready(function () {
+  $('DIV#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
+});
