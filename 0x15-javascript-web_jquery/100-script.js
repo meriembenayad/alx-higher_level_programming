2610411f9f46c3c9a53b524931044cee13582a6b
meriembenayad <meriem.benayad@gmail.com>
2024-01-10 20:02:09 +0100

    Task 10: No jQuery - document loaded

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+/* JavaScript script that updates the text color of the <header>
+  using querySelector
+ */
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
+
